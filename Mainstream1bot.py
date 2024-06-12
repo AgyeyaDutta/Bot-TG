b6029bb2a27b6eb74507777cbeb49c367137541b
AgyeyaDutta <aldehydeketone31@gmail.com>
2024-06-12 13:54:48 +0530

    c1

---
+++ Mainstream1bot.py
@@ -0,0 +1,26 @@
+import telegram.ext
+import telegram
+import pandas_datareader.data as web
+import cryptocompare
+import os
+with open('C:\\Users\\Agyeya\\Desktop\\TB\\Codes\\token.txt','r') as f:
+    TOKEN = f.read()
+
+def start(update, context):
+    update.message.reply_text("""send in format- /crypto 'coin name'""")
+
+
+def crypto(update, context):
+    ticker = context.args[0]
+    data = cryptocompare.get_price(ticker, 'USD')
+    update.message.reply_text(f"{data}")
+
+
+updater=telegram.ext.Updater(TOKEN, use_context=True)
+dp= updater.dispatcher
+
+dp.add_handler(telegram.ext.CommandHandler("start",start))
+dp.add_handler(telegram.ext.CommandHandler("crypto",crypto))
+
+updater.start_polling()
+updater.idle()
